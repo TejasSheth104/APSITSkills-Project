41d9082a4ddd2ffb7589379917369978134a3942
ParagPatil11 <68297157+ParagPatil11@users.noreply.github.com>
2020-07-15 23:50:19 +0530

    Add files via upload

---
+++ PrimeOrComposite.py
@@ -0,0 +1,16 @@
+#(3)PRIME OR COMPOSITE
+number = int(input("Enter any number: "))
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(number, "is a composite number")
+            break
+    else:
+        print(number, "is a prime number")
+
+
+else:
+    print(number, "is a composite number")
+    
+
+
