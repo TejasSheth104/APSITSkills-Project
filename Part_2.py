f20c9b56d885ffcccc5b0e5b0c1f429dade58e24
Tejas A. Sheth <52593382+TejasSheth104@users.noreply.github.com>
2020-07-16 18:23:31 +0530

    part 2.

--- Part_2.py
+++ Part_2.py
@@ -1,6 +1,8 @@
 # Write a Python program to find whether a given number (accept from the user) 
 # is even or odd, print out an appropriate message to the user.
 
+# even numbers = all those numbers that are completely divisble by 2, including 0. 
+
 def even_odd():
     while True:
         print('\nEnter \'-1\' to QUIT.')
@@ -8,12 +10,15 @@ def even_odd():
         try:
             number = int(number)
         except:
+            # if user enters anything other than an integer, the below message pops up
             print('\tINTERGER EXCEPTED\n\tTRY AGAIN.\n')
             continue
         if number == -1:
+            # exit condition
             print('\tTHANK YOU.\n\tRETURNING TO MAIN MENU.\n')
             break
         elif number < -1:
+            # all negative numbers are not classified as odd or even
             print('Neither ODD nor EVEN\n')
         elif number % 2 == 0:
             print('Number', number, '- is EVEN\n')
