3c6cdb6cda4299c4ff8e75ff441efb47890c66f8
Tejas A. Sheth <52593382+TejasSheth104@users.noreply.github.com>
2020-07-20 00:35:53 +0530

    Part 1

--- Part_1.py
+++ Part_1.py
@@ -4,20 +4,25 @@
 
 def all_prime():
     while True:
-        print('\nEnter \'-1\' to QUIT.')
+        print('\nEnter \'-1\' to GO BACK.')
         start = input('Enter the Start Number - ')
         end = input('Enter the End Number - ')
         try:
             start = int(start)
             end = int(end)
         except:
+            # if user enters anything other than an integer, the below message pops up
             print('\tINVALID CHOICE.\n\tTRY AGAIN.\n')
             continue
         if start == -1 or end == -1:
             # exit condition
             print('\tTHANK YOU.\n\tRETURNING TO MAIN MENU.\n')
             break
-        for num in range(start, end):
+        if start <= end:
+            # start value should be always less than the end value 
+            print('\tSTART SHOULD BE ALWAYS LESS THAN END\n\tTRY AGAIN.\n')
+            continue
+        for num in range(start, end + 1):
             if num > 1:
                 for i in range(2, num):
                     if num % i == 0:
